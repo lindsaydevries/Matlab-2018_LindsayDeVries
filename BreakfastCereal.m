1b1e07aaa9ff55635e3e2a74eac460668278feca
lindsaydevries <35048919+lindsaydevries@users.noreply.github.com>
2018-01-03 12:24:21 -0800

    Add files via upload

---
+++ BreakfastCereal.m
@@ -0,0 +1,31 @@
+
+% BreakfastCereal
+%
+% Provides examples of fun things you can do with cereal boxes.
+%Inspired by my cats
+%
+%written LD 1/3/2018 for Matlab 2018
+
+frosted ='FROSTED FLAKES';
+cherrios = 'CHEERIOS'; 
+
+frosted(9)  
+
+frosted_scrambled = frosted; 
+
+frosted_scrambled(9) = 'E'; 
+disp(frosted_scrambled); 
+
+cheerios = 'CHEERIOS'; 
+cheerios_scrambled = cheerios; 
+
+cheerios_scrambled(1) = 'O'; 
+cheerios_scrambled(5) = 'P'; 
+disp(cheerios_scrambled); 
+
+chemistry = 'CHEMISTRY';
+chem_scrambled = chemistry;
+
+chem_scrambled(1) = 'O'; 
+chem_scrambled(8) = 'B'; 
+disp(chem_scrambled); 
